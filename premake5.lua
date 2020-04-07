fb91fe89b3d439ed64f8b3c1b33b13a7ee4fff6e
mateimicro123 <34721984+mateimicro123@users.noreply.github.com>
2020-04-07 13:40:38 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,51 @@
+project "GLFW"
+    kind "StaticLib"
+    language "C"
+    targetdir ("bin/" .. outputdir .. "/GLFW")
+    objdir ("bin-int/" .. outputdir .. "/GLFW")
+
+    files
+    {
+        "include/GLFW/glfw3.h",
+        "include/GLFW/glfw3native.h",
+        "src/glfw_config.h",
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/vulkan.c",
+        "src/window.c"
+    }
+    --Linux specific files (X11)
+    --TODO: Add Windows support
+    filter "system:linux"
+        pic "On"
+
+        systemversion "latest"
+        staticruntime "On"
+
+        files
+        {
+            "src/x11_init.c",
+            "src/x11_monitor.c",
+            "src/x11_window.c",
+            "src/xkb_unicode.c",
+            "src/posix_time.c",
+            "src/posix_thread.c",
+            "src/glx_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c",
+            "src/linux_joystick.c"
+        }
+
+        defines "_GLFW_X11"
+
+        filter "system:linux"
+            pic "On"
+
+        filter "configurations:Debug"
+            runtime "Debug"
+            symbols "On"
+        filter "configurations:Release"
+            runtime "Release"
+            optimize "On"
